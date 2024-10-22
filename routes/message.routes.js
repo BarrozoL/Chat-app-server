652dd32fa0bfea249f107642057e0c5cd66dd4a5
BarrozoL <barrozo.lucasp@gmail.com>
2024-10-22 19:03:10 +0100

    able to send messages correctly

--- routes/message.routes.js
+++ routes/message.routes.js
@@ -6,15 +6,17 @@ const User = require("../models/User.model.js");
 //Post a message
 router.post("/chat", async (req, res, next) => {
   try {
+    //Get text, sender and receiver from the request body
     const { text, sender, receiver } = req.body;
 
+    //Check if text, sender and receiver are provided
     if (!text || !sender || !receiver) {
       res.status(400).json({
         message: "Message requires text, sender, receiver",
       });
       return;
     }
-
+    //Check if conversation exists between the two users
     let conversation = await Conversation.findOne({
       $or: [
         { user1Id: sender, user2Id: receiver },
@@ -22,11 +24,13 @@ router.post("/chat", async (req, res, next) => {
       ],
     });
 
+    //If no conversation exists, create a new conversation and push it to the users conversations array
     if (!conversation) {
       conversation = await Conversation.create({
         user1Id: sender,
         user2Id: receiver,
         members: [sender, receiver],
+        messages: [],
       });
 
       await Promise.all([
@@ -40,9 +44,9 @@ router.post("/chat", async (req, res, next) => {
     }
 
     const newMessage = await Message.create({
-      text,
-      sender,
-      receiver,
+      text: text,
+      sender: sender,
+      receiver: receiver,
       conversation: conversation._id,
     });
 
