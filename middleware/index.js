73fe5e65898807955bdb0ca18ddf0267b83776a7
BarrozoL <barrozo.lucasp@gmail.com>
2024-10-22 01:57:09 +0100

    conversation and message models

--- middleware/index.js
+++ middleware/index.js
@@ -7,21 +7,22 @@ const isAuthenticated = require("./isAuthenticated.middleware");
 
 //Routes
 const indexRoutes = require("../routes/index.routes");
-const authRoutes = require("../routes/user.routes");
+const userRoutes = require("../routes/user.routes");
 const protectedRoutes = require("../routes/protected.routes");
 
 module.exports = (app) => {
   app.use(
     cors({
-      origin: process.env.ORIGIN || "http://localhost:3000",
+      origin: process.env.ORIGIN || "http://localhost:5173",
       credentials: true,
     })
   );
 
   app.use(logger("dev"));
   app.use(express.json());
+
+  //Routes
   app.use("/", indexRoutes);
-  app.use("/", indexRoutes);
-  app.use("/auth", authRoutes);
+  app.use("/auth", userRoutes);
   app.use("/protected", verifyToken, protectedRoutes);
 };
---
+++ routes/message.routes.js
@@ -0,0 +1,6 @@
+const router = require("express").Router();
+const Message = require("../models/Message.model.js");
+const Conversation = require("../models/Conversation.model.js");
+const User = require("../models/User.model.js");
+
+//Post a message
